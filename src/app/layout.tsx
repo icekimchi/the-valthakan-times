984b59a9804816f31ee4695d5923e965fdae3449
icekimchi <pjh010828@gmail.com>
2025-10-29 00:45:53 -0700

    style: add Italianno header font and set Helvetica as default body font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Italianno, Eczar } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const italianno = Italianno({
+  weight: "400",
+  variable: "--font-italianno",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const eczar = Eczar({
   subsets: ["latin"],
+  variable: "--font-eczar",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${italianno.variable} ${eczar.variable} antialiased`}
       >
         {children}
       </body>
