050546b96b695f0c065dbf0398f849ce27b9f1cb
icekimchi <pjh010828@gmail.com>
2025-11-17 00:49:50 -0800

    feat: update newsletter page layout and styling

--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -1,24 +1,32 @@
 import LatestEdition from "@/components/LatestEdition";
 import Navbar from "@/components/Navbar";
 import SearchBar from "@/components/SearchBar";
+import { fetchPagedList } from "@/lib";
+import NewsletterList from "@/components/NewsletterList";
 
 export default async function Page() {
+  
+    const { items } = await fetchPagedList({ page: 1, limit: 12 });
   return (
     <>
       <Navbar />
-
-      <main className="relative pt-20">
-        <div className="w-full text-center">
-          <h1 className="text-white heading-sp-h1 [text-shadow:_0px_0px_40px_rgb(147_112_219_/_0.50)]">
+      <main className="relative pt-20 gap-10">
+        <div className="w-full text-center py-10">
+          <h1 className="text-white text-[56px] [text-shadow:_0px_0px_40px_rgb(147_112_219_/_0.50)]">
               Featured Posts
           </h1>
         </div>
 
-        <div className="flex w-full justify-center mb-8 z-20">
+        <div className="inline-flex flex-col items-center w-full justify-center gap-8">
           <SearchBar />
+          <LatestEdition />
         </div>
 
-        <LatestEdition />
+        
+
+        <div className="flex w-full justify-center mb-8 z-20">
+          <NewsletterList items={items} />
+        </div>
       </main>
 
     </>
