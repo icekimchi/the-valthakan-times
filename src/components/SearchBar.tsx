df6f0faec0a767215e5eeaa59d78fceab4523c2b
icekimchi <pjh010828@gmail.com>
2025-11-21 09:42:40 -0800

    style: change page

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -28,7 +28,7 @@ export default function SearchBar({
         e.preventDefault();
         if (onSubmit) onSubmit(value ?? "");
       }}
-      className="h-[52px]"
+      className="w-full h-[52px]"
     >
       <div
         className="group flex h-full w-full items-center gap-4 rounded-lg border border-[#302D9A]/30 bg-white/5 px-4 backdrop-blur-sm transition focus-within:border-[#302D9A]/50"
